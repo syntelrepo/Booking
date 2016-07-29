df2489809264b5dbecc2581b43fb403bf84d7fd1
unknown <A25481@SYNCGDC3446.syntbots.com>
2016-07-29 12:34:46 +0530

    testing purpose

---
+++ Cart/src/main/java/GetName/CustName.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * 2016, All rights reserved.
+ *******************************************************************************/
+package GetName;
+
+import GetName.CustName;
+// Start of user code (user defined imports)
+
+// End of user code
+
+/**
+ * Description of CustName.
+ * 
+ * @author A25481
+ */
+/*package*/ class CustName {
+	/**
+	 * Description of the property Name.
+	 */
+	public CustName Name = null;
+
+	// Start of user code (user defined attributes for CustName)
+
+	// End of user code
+
+	/**
+	 * The constructor.
+	 */
+	public CustName() {
+		// Start of user code constructor for CustName)
+		super();
+		// End of user code
+	}
+
+	/**
+	 * Description of the method GoldCustName.
+	 * @param  
+	 */
+	public void GoldCustName(CustName ) {
+		// Start of user code for method GoldCustName
+		// End of user code
+	}
+
+	// Start of user code (user defined methods for CustName)
+
+	// End of user code
+	/**
+	 * Returns Name.
+	 * @return Name 
+	 */
+	public CustName getName() {
+		return this.Name;
+	}
+
+	/**
+	 * Sets a value to attribute Name. 
+	 * @param newName 
+	 */
+	public void setName(CustName newName) {
+		this.Name = newName;
+	}
+
+}
